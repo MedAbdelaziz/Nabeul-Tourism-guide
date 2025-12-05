6f82604a6222d932399b06c4fb30b551ab60932c
Med Abdelaziz <medabdelaziz.benzina@gmail.com>
2025-12-05 08:57:09 +0100

    Hotels database sample

---
+++ data/hotel.js
@@ -0,0 +1,18 @@
+export let hotels=[
+    {
+        id:`1`,
+        name:`The Grand Budapest OCEANA Hotel & Spa`,
+        image:`/images/hotels/oceana.jpg`,
+        description:`Hotel Oceana Hammamet Adults Only is set in lush gardens of palm trees 
+        right on the beach front.It offers a wellness centre, swimming pools,
+         sauna and four restaurants.`,
+         price:120,
+         rating :{
+            score:9.1,
+            reviews:493,
+            note:`Very Good`,
+            starts:5
+         }
+
+    }
+]
