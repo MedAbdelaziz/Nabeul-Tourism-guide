73cfa75b7986ba934d10f542d9cfc28636bdb2f9
Med Abdelaziz <medabdelaziz.benzina@gmail.com>
2025-12-23 12:18:57 +0100

    fix accommodation bugs

--- data/hotel.js
+++ data/hotel.js
@@ -2,7 +2,7 @@ export const hotels=[
     {
         id:`1`,
         name:`The Grand Budapest OCEANA Hotel & Spa`,
-        image:`/images/hotels/oceana-main.jpg`,
+        image:`images/hotels/oceana-main.jpg`,
         description:`Hotel Oceana Hammamet Adults Only is set in lush gardens of palm trees 
         right on the beach front.It offers a wellness centre, swimming pools,
          sauna and four restaurants.`,
@@ -12,32 +12,39 @@ export const hotels=[
             reviews:493,
             note:`Very Good`,
             stars:5
-         },
-        id:`2`,
-        name:`Royal Tulip Taj Sultan`,
-        image:`/images/hotels/Taj Sultan.jpg`,
-        description:`Featuring direct access to Hammamet Beach, the 4-star Le Sultan hotel has 2 swimming pools.
-         Hammamet city center is 3 km away.`,
-         price:150,
-         rating :{
-            score:8.1,
-            reviews:980,
-            note:`Good`,
-            stars:4
-         },
-          id:`3`,
-        name:`The Mirage Resort & Spa`,
-        image:`/images/hotels/mirage.jpg`,
-        description:`The Mirage Resort & SPA is located in Hammamet, 5 km from Yasmine Hammamet. 
-        This resort features a private beach, a restaurant, and a bar.`,
-         price:180,
-         rating :{
-            score:8.3,
-            reviews:1182,
-            note:` Very Good`,
-            stars:4
-         },
-          id:`4`,
+         }},
+         {
+            id:`2`,
+            name:`Royal Tulip Taj Sultan`,
+            image:`images/hotels/Taj-Sultan-main.jpg`,
+            description:`Featuring direct access to Hammamet Beach, the 4-star Le Sultan hotel has 2 swimming pools.
+               Hammamet city center is 3 km away.`,
+               price:150,
+               rating :{
+                  score:8.1,
+                  reviews:980,
+                  note:`Good`,
+                  stars:4
+               }
+         }
+        ,
+        {
+            id:`3`,
+            name:`The Mirage Resort & Spa`,
+            image:`images/hotels/mirage-main.jpg`,
+            description:`The Mirage Resort & SPA is located in Hammamet, 5 km from Yasmine Hammamet. 
+            This resort features a private beach, a restaurant, and a bar.`,
+               price:180,
+               rating :{
+                  score:8.3,
+                  reviews:1182,
+                  note:` Very Good`,
+                  stars:4
+               }
+        }
+          ,
+      {
+         id:`4`,
         name:`Iberostar Waves Averroes`,
         image:`/images/hotels/iberostar.jpg`,
         description:`Iberostar Waves Averroes is a seaside resort located in Yasmine Hammamet,
@@ -48,20 +55,26 @@ export const hotels=[
             reviews:1664,
             note:` Very Good`,
             stars:4
-         },
-            id:`5`,
-        name:`La Badira-Adult Only`,
-        image:`/images/hotels/badira.jpg`,
-        description:`Located in Hammamet, La Badira-Adult Only features 3 outdoor swimming pools and access to a private beach.
-         Free Wi-Fi is provided.`,
-         price:200,
-         rating :{
-            score:9.1,
-            reviews:1560,
-            note:` Fabulous`,
-            stars:5
-         },
-            id:`6`,
+         }
+      }
+      ,
+      {
+         id:`5`,
+         name:`La Badira-Adult Only`,
+         image:`/images/hotels/badira.jpg`,
+         description:`Located in Hammamet, La Badira-Adult Only features 3 outdoor swimming pools and access to a private beach.
+            Free Wi-Fi is provided.`,
+            price:200,
+            rating :{
+               score:9.1,
+               reviews:1560,
+               note:` Fabulous`,
+               stars:5
+            }
+      }
+      ,
+      {
+         id:`6`,
         name:`Royal Azur Thalasso`,
         image:`/images/hotels/azur.jpg`,
         description:`Royal Azur Thalassa welcomes you in Hammamet, just steps away from Hammamet Beaches.`,
@@ -71,8 +84,19 @@ export const hotels=[
             reviews:1770,
             note:` Fabulous`,
             stars:5
-         },
-
-
-    }
+         }
+      },
+      {
+         id:`7`,
+        name:`Luxury Appartments`,
+        image:`/images/hotels/lux-apartment.jpg`,
+        description:`Royal Azur Thalassa welcomes you in Hammamet, just steps away from Hammamet Beaches.`,
+         price:190,
+         rating :{
+            score:9.0,
+            reviews:1770,
+            note:` Fabulous`,
+            stars:5
+         }
+      }
 ]
