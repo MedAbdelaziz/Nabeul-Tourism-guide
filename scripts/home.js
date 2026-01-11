ce3b5aab50a030481e5e9b09005479cb96d9cf6a
Med Abdelaziz <medabdelaziz.benzina@gmail.com>
2026-01-11 10:00:40 +0100

    Improve nav bar

--- scripts/home.js
+++ scripts/home.js
@@ -3,11 +3,30 @@ import { addReservation,reservations } from '../data/reservation.js';
 const reservationForm = document.querySelector('.reservation-form');
 reservationForm.addEventListener('submit', (event)=> {
     event.preventDefault();
-
     const name = document.getElementById('name').value ;
+    let pattern=/(^[A-Z]?[a-z]+\s){2,}/;
+    if(!pattern.test(name)){
+        alert('Invalid name');
+        reservationForm['name'].style.border="red solid 2px";
+        return;
+    }
     const email = document.getElementById('email').value ;
+    pattern=/^\w+([\.-_]?\w+)*@\w+([\.-_]?\w+)*(\.\w{2,3})+$/;
+    if(!pattern.test(email)){
+        alert('Invalid email');
+        reservationForm['name'].style.border="red solid 2px";
+        return;
+    }
     const date = document.getElementById('date').value ;
+    if(){
+        alert('too young');
+        return;
+    }
     const guests = document.getElementById('guests').value ;
+    pattern=/[0-9]/;
+    if(!pattern.test(guests)){
+        alert('Number of guest invalid');
+    }
 
     const newReservation = { name, email, date, guests};
     console.log(newReservation);
